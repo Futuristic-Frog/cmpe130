ac74cefbde06b9482f3eecbede80fdc8adb963a7
Mauricio Curiel <mauriciom.curiel@gmail.com>
2023-03-26 14:21:45 -0700

    adds navbar unstyled

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class navbar extends Component {
+  render() {
+    return (
+        <>
+            <h1>Watsonville</h1>
+            <div>
+                <a href="www.google.com">Home</a>
+                <a href="www.google.com">Log In</a>
+                <a href="www.google.com">Community Showcase</a>
+                <a href="www.google.com">Events</a>
+                <a href="www.google.com">Bulletin Board</a>
+                <a href="www.google.com">FAQ</a>
+            </div>
+        </>
+    )
+  }
+}
