8ddd8ed13e89aab08248f1ebc5ff24581c59f137
Mauricio Curiel <mauriciom.curiel@gmail.com>
2023-03-26 15:22:24 -0700

    styles navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './component.css';
 
 export default class navbar extends Component {
   render() {
     return (
         <>
-            <div>
+            <div className='navbar'>
                 <h1>Wiliamson</h1>
                 <a href="#home">Home</a>
                 <a href="#login">Log In</a>
